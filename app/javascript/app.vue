9a8b4cb2ba987a23218c2f14e46bd047966ad99a
take-paolo <42969542+take-paolo@users.noreply.github.com>
2021-11-02 14:14:09 +0900

    Add: Vue Router

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,5 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <router-view />
   </div>
 </template>
-
-<script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
-</script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -7,9 +7,11 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import router from '../router/index.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
+    router,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
