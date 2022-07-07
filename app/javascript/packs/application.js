664e47182e760f7c24fb4ae477f2fcb85a479c3c
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-08 01:35:49 +0900

    Delete: turbolinks loading

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,8 @@
 // that code so it'll be compiled.
 
 import Rails from '@rails/ujs'
-import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import 'channels'
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
