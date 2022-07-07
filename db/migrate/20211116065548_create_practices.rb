5f8ddc5ef30d708aa41401b96e75efbfe138f8fa
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-07 23:47:53 +0900

    Add: migration file for sqlab

---
+++ db/migrate/20211116065548_create_practices.rb
@@ -0,0 +1,18 @@
+class CreatePractices < ActiveRecord::Migration[6.1]
+  def change
+    create_table :practices do |t|
+      t.references :chapter, null:false, foreign_key: true
+      t.string :name, null: false
+      t.text :question, null: false
+      t.text :answer, null: false
+      t.string :sample_database_uid, null:false
+      t.boolean :display_er_diagram, null: false, default: true
+      t.boolean :enabled, null: false, default: false
+      t.boolean :published, null: false, default: false
+      t.boolean :requires_auth, null: false, default: true
+      t.integer :order_number, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20211116065620_create_sample_tables.rb
@@ -0,0 +1,12 @@
+class CreateSampleTables < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sample_tables do |t|
+      t.references :practice, null:false, foreign_key: true
+      t.string :uid, null:false
+
+      t.timestamps
+    end
+
+    add_index :sample_tables, [:practice_id, :uid], unique: true
+  end
+end
