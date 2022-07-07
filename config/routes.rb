708029bbe913f9c17b9e4f9e68a659c3a3b38f9f
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-08 03:53:03 +0900

    Add: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '*path', to: 'home#index'
+  namespace :api do
+    resources :works, param: :slug, only: %i[index show]
+    resources :practices, only: %i[show]
+
+    namespace :samples do
+      resources :databases, controller: 'model_databases', only: %i[index show] do
+        post 'sql', to: "queries#index"
+        resources :tables, controller: 'model_tables', only: %i[index]
+      end
+    end
+  end
+  get '*path', to: 'home#index', constraints: lambda { |req| req.path.exclude? 'rails/active_storage' }
 end
