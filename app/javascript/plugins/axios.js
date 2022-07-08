8f286b406457c0df20b9a4fb82d1b0f2c3b2f305
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-09 02:45:58 +0900

    Change: axios setting

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
-const axiosInstance = axios.create({
-  baseURL: 'api',
-})
+const production = ''
+const development = 'http://127.0.0.1:3000'
+const url = process.env.NODE_ENV === 'production' ? production : development
+
+const axiosInstance = axios.create({ baseURL: `${url}/api` })
+
+if (localStorage.auth_token) {
+  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.auth_token}`
+}
 
 export default axiosInstance
