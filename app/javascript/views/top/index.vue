0cba15fd24a9bba08ef059b6cd0711eadb3a316e
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-09 03:00:49 +0900

    Add: top view

---
+++ app/javascript/views/top/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>SQLab</h1>
+    <router-link :to="{ name: 'Works' }">問題集一覧</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopView',
+}
+</script>
