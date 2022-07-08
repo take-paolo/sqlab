36b7744f1b1abf5372d724c396240d1670e13e50
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-09 02:59:26 +0900

    Add: error views

---
+++ app/javascript/views/errors/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <router-link :to="{ name: 'Works' }">問題集一覧に戻る</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+}
+</script>
