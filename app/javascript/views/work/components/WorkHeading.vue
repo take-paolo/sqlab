b8bdddf5f2c21a5eec11986da75755d3eeebb3bc
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-12 03:13:33 +0900

    Change: work views

---
+++ app/javascript/views/work/components/WorkHeading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-h1 text-center font-weight-black font--text text--darken-1 py-8">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkHeading',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
