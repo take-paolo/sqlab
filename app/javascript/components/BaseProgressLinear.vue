e65e38406aa1a9a9db15139ba2764cc74f3d3eb3
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-12 03:12:23 +0900

    Add: base component

---
+++ app/javascript/components/BaseProgressLinear.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-progress-linear
+    :value="value"
+    :height="height"
+    rounded
+    background-color="grey lighten-3"
+    v-bind="$attrs"
+  >
+    <span class="text-default">{{ value }}%</span>
+  </v-progress-linear>
+</template>
+
+<script>
+export default {
+  name: 'BaseProgressLinear',
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: [String, Number],
+      default: 0,
+    },
+    height: {
+      type: [String, Number],
+      default: 25,
+    },
+  },
+}
+</script>
