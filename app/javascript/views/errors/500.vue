97cec547622b10d73b302b6e35181acf693e61ae
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-07-21 18:28:43 +0900

    Change: errors view design

--- app/javascript/views/errors/500.vue
+++ app/javascript/views/errors/500.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <h1>500</h1>
-    <router-link :to="{ name: 'Works' }">問題集一覧に戻る</router-link>
-  </div>
+  <ErrorsContent img="internal-server-error.svg">
+    <template #title>500</template>
+    <template #subtitle>Internal Server Error</template>
+    <template #text>
+      システムエラーが発生しました。<br />
+      お手数ですが、しばらく時間をおいてから再度お試しください。
+    </template>
+  </ErrorsContent>
 </template>
 
 <script>
+import ErrorsContent from './components/ErrorsContent'
+
 export default {
   name: 'InternalServerError',
+  components: {
+    ErrorsContent,
+  },
 }
 </script>
