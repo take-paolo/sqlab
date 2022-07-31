8dc67adb4abcdb335f66af9f39adfc9a4c3fd3b8
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-08-01 02:11:25 +0900

    Add: base component

---
+++ app/javascript/components/BaseContainerAdmin.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container
+    class="base-container-admin"
+    fluid
+    v-bind="$attrs"
+  >
+    <slot />
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'BaseContainerAdmin',
+  inheritAttrs: false,
+}
+</script>
+
+<style lang="scss" scoped>
+.base-container-admin {
+  padding: 60px 10%;
+  max-width: 1920px;
+}
+</style>
