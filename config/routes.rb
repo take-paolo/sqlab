469c6eef40f833c44b58e361258db58dae4bb6ed
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-08-01 01:55:24 +0900

    Add: admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,21 @@ Rails.application.routes.draw do
         resources :tables, controller: 'model_tables', only: %i[index]
       end
     end
+
+    namespace :admin do
+      namespace :works do
+        resource :order, only: %i[update]
+      end
+      resources :works, only: %i[index create update destroy]
+      namespace :chapters do
+        resource :order, only: %i[update]
+      end
+      resources :chapters, only: %i[index create update destroy]
+      namespace :practices do
+        resource :order, only: %i[update]
+      end
+      resources :practices, only: %i[index create update destroy]
+    end
   end
   get '*path', to: 'home#index', constraints: lambda { |req| req.path.exclude? 'rails/active_storage' }
 end
