35e875a323c7e7571c175cb38f8201050878dedf
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-08-01 01:53:11 +0900

    Add: sort method

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -11,5 +11,16 @@ module Sortable
     def set_order_number
       self.order_number = self.class.maximum(:order_number).to_i + 1
     end
+
+    class << self
+      def update_order(ids_arr)
+        ids_arr.each do |ids|
+          ids.each_with_index do |id, index|
+            record = find(id)
+            record.update!(order_number: index)
+          end
+        end
+      end
+    end
   end
 end
