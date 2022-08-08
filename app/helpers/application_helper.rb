4267d832b5a2df6b0ce1bc0e7126ac7b4e3ce93b
take-paolo <42969542+take-paolo@users.noreply.github.com>
2022-08-08 15:28:59 +0900

    Add: default meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'SQLab',
+      title: 'SQL特化の学習サービス',
+      description: 'SQLの練習ができるSQL特化の学習サービスです。環境構築不要で豊富な練習問題に取り組むことができます。SQLabを活用してSQLの知識をスキルに変えていきましょう。',
+      keywords: 'sqlab, sql, sql 練習, sql 演習, sql 問題, sql 勉強',
+      charset: 'utf-8',
+      separator: '|',
+      canonical: request.original_url,
+      icon: [
+        { href: image_url('/favicon.ico') },
+        { href: image_url('/apple-touch-icon-precomposed.png'), rel: 'apple-touch-icon-precomposed', sizes: '180x180', type: 'image/png' }
+      ],
+      og: {
+        site_name: :site,
+        title: 'SQLab | SQL特化の学習サービス',
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('/ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        title: 'SQLab | SQL特化の学習サービス',
+        description: :description,
+        image: image_url('/ogp.png'),
+        card: 'summary_large_image',
+        site: '@sqlab_app'
+      }
+    }
+  end
 end
