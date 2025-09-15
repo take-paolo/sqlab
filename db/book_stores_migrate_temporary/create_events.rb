b2988369cfe90c8e80add9711d922bf390baa68e
Yuki Takeuchi <yuki.takeuchi@Yukis-MacBook-Air.local>
2025-09-15 11:55:30 -0700

    refactor: update temporary migration structure
    Change: inherited class name Collections::TempMigration → TemporaryMigration
    Change: method name temp_up → create_temporary_table

--- db/book_stores_migrate_temporary/create_events.rb
+++ db/book_stores_migrate_temporary/create_events.rb
@@ -1,5 +1,5 @@
-class CreateEvents < Samples::Collections::TempMigration
-  def temp_up
+class CreateEvents < Samples::TemporaryMigration
+  def create_temporary_table
     create_table :events, temporary: true, force: true do |t|
       t.string :name, null: false
       t.integer :max_num, null: false
